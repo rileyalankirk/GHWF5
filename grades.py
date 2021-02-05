9dd1f50e3d58587bf910c2de8db91ba161032cb7
Jonah Beers <beersj02@moravian.edu>
2021-02-05 15:55:18 -0500

    Add grade average for two grades
    Created test for average of two grades. Function computes average of two or fewer grades.

--- grades.py
+++ grades.py
@@ -2,4 +2,6 @@
 def compute_hw_average(grades):
     if len(grades) == 0:
         return 0
-    return grades[0]
+    if len(grades) == 1:
+        return grades[0]
+    return (grades[0] + grades[1]) / 2
